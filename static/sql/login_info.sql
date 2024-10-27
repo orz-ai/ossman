6d1ca8bb0a8a95e5cece372966f99693c16098f6
Gang <deepincode@qq.com>
2024-10-27 22:21:07 +0800

    fix: add resource files

---
+++ static/sql/login_info.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE IF NOT EXISTS "login_info" (
+  "id" INTEGER NOT NULL,
+  "name" TEXT NOT NULL,
+  "secret_id" TEXT NOT NULL,
+  "secret_key" TEXT NOT NULL,
+  "remark" TEXT,
+  "timestamp" integer NOT NULL,
+  PRIMARY KEY ("id"),
+  CONSTRAINT "uk_secret_id" UNIQUE ("secret_id"),
+  CONSTRAINT "uk_name" UNIQUE ("name")
+);
